d69f43973ffa6f04d413e9e4d4f8b478eb1fca33
Akmaral <akmaral1996@mail.ru>
2019-09-26 16:28:07 +0600

    Some fix

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -22,23 +22,23 @@ $(document).ready(function () {
         arrows: true
     });
     $('#section1_causel-desktop').slick({
-        autoplay: false,
-        autoplaySpeed: 3000,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: false,
-        arrows: true
-    });
-    $('#section1_causel-mob').slick({
-        autoplay: false,
-        autoplaySpeed: 3000,
+        autoplay: true,
+        autoplaySpeed: 2000,
         slidesToShow: 1,
         slidesToScroll: 1,
         dots: true,
         arrows: false
     });
-    $('.rows').slick({
-        autoplay: false,
+    $('#section1_causel-mob').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false
+    });
+    $('.rows-desktop').slick({
+        autoplay: true,
         autoplaySpeed: 3000,
         slidesToShow: 3,
         slidesToScroll: 1,
